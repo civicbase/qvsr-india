2890f7e6cad89b1985a77d91153f2d989506ff4d
Alan Mortoni <mortoni.alan@hotmail.com>
2022-07-20 21:50:06 +1000

    fix: Rating button to be type button

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import tw, { theme } from 'twin.macro'
 import { AiFillStar, AiOutlineStar } from 'react-icons/ai'
+import Button from 'components/Button'
 
 const Rating = () => {
   const [rate, setRate] = useState(0)
 
   return (
     <div css={tw`flex justify-center  p-8 pb-2 w-auto space-x-3`}>
-      <button onClick={() => setRate(1)}>
+      <button onClick={() => setRate(1)} type="button">
         {rate >= 1 ? (
           <AiFillStar size={22} color={theme`colors.bgColor9`} />
         ) : (
@@ -15,7 +16,7 @@ const Rating = () => {
         )}
       </button>
 
-      <button onClick={() => setRate(2)}>
+      <button onClick={() => setRate(2)} type="button">
         {rate >= 2 ? (
           <AiFillStar size={22} color={theme`colors.bgColor9`} />
         ) : (
@@ -23,7 +24,7 @@ const Rating = () => {
         )}
       </button>
 
-      <button onClick={() => setRate(3)}>
+      <button onClick={() => setRate(3)} type="button">
         {rate >= 3 ? (
           <AiFillStar size={22} color={theme`colors.bgColor9`} />
         ) : (
@@ -31,7 +32,7 @@ const Rating = () => {
         )}
       </button>
 
-      <button onClick={() => setRate(4)}>
+      <button onClick={() => setRate(4)} type="button">
         {rate >= 4 ? (
           <AiFillStar size={22} color={theme`colors.bgColor9`} />
         ) : (
@@ -39,7 +40,7 @@ const Rating = () => {
         )}
       </button>
 
-      <button onClick={() => setRate(5)}>
+      <button onClick={() => setRate(5)} type="button">
         {rate >= 5 ? (
           <AiFillStar size={22} color={theme`colors.bgColor9`} />
         ) : (
