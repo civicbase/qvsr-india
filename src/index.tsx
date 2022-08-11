89950ad3c17e0631674891ea4f1a7a830aa5b0a2
Alan Mortoni <mortoni.alan@hotmail.com>
2022-08-11 17:58:05 +1000

    feat: service work

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import GlobalStyles from './styles/GlobalStyles'
 import App from './App'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 const container = document.getElementById('root')
 const root = createRoot(container!)
@@ -12,3 +13,8 @@ root.render(
     <App />
   </React.StrictMode>,
 )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register()
